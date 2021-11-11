b4897eb7a7fb1c32820c6b399cc80549f42eedc1
DartRock <64318620+DartRock@users.noreply.github.com>
2021-11-11 19:03:57 +0200

    fixes 6th task

--- js/task-06.js
+++ js/task-06.js
@@ -1,16 +1,52 @@
 const inputRef = document.querySelector('[data-length="6"]');
 const attr = inputRef.dataset.length;
+
+const addValidClass = (classToAdd, classToremove) => {
+  inputRef.classList.add(classToAdd);
+  inputRef.classList.remove(classToremove);
+};
+
 inputRef.addEventListener("blur", (e) => {
-  if (e.currentTarget.value.length === 0) {
-    inputRef.classList.remove("invalid");
-    inputRef.classList.remove("valid");
+  const length = e.currentTarget.value.length;
+
+  // if (length === 0) {
+  //   // return addAndRemoveClasses("removeAll");
+  // }
+  if (length < attr) {
+    return addValidClass("invalid", "valid");
+    // return addAndRemoveClasses("invalid");
   }
-  if (e.currentTarget.value.length < attr && e.currentTarget.value.length > 0) {
-    inputRef.classList.add("invalid");
-    inputRef.classList.remove("valid");
-  }
-  if (e.currentTarget.value.length >= attr) {
-    inputRef.classList.remove("invalid");
-    inputRef.classList.add("valid");
+  if (length >= attr) {
+    return addValidClass("valid", "invalid");
+    // return addAndRemoveClasses("valid");
   }
 });
+
+// Чтобы сбрасывались все классы если число символо = 0
+// const addClass = (classToAdd) => {
+//   return inputRef.classList.add(classToAdd);
+// };
+// const removeClass = (classToRemove) => {
+//   return inputRef.classList.remove(classToRemove);
+// };
+
+// const addAndRemoveClasses = (value) => {
+//   console.log(value);
+//   if (value === "removeAll") {
+//     removeClass("valid");
+//     removeClass("invalid");
+//     return;
+//   }
+
+//   if (value === "invalid") {
+//     removeClass("valid");
+//     addClass("invalid");
+//     return;
+//   }
+
+//   if (value === "valid") {
+//     removeClass("invalid");
+//     addClass("valid");
+//     return;
+//   }
+// };
